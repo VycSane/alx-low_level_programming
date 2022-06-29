b77aea0679e693434fd1db3ddcef5c6bf2fe6f1b
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-06-29 10:39:33 -0700

    Added thr printf.c source code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char note[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s\n", note);
+	return (0);
+}
