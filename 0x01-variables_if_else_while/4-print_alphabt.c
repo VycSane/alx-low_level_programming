7d01f38a19588796a1a5bd50c6e9bcb0c9635932
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-01 05:24:55 -0700

    Added the 4-print_alphabt.c source code

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; ++letter)
+	{
+		if (letter == 'e' || letter == 'q')
+			continue;
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
