5356e96d14dd779093683c12960b54fd331fbda3
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-01 05:55:03 -0700

    Added the 5-print_numbers.c source code

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int count;
+
+	for (count = 0; count < 10; count++)
+	{
+		printf("%d", count);
+	}
+	printf("\n");
+	return (0);
+}
