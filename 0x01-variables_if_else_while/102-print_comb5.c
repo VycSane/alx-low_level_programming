017f498463254c382aba29641c34297120462f26
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-01 16:47:56 -0700

    Correction

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,15 +18,18 @@ int main(void)
 			{
 				for (l = '1'; l <= '9'; ++l)
 				{
+					if ((k >= i && l >= j) && !(i == k && j == l))
+					{
 					putchar(i);
 					putchar(j);
 					putchar(' ');
 					putchar(k);
 					putchar(l);
-					if (i == '9' && j == '9' && k == '9' && l == '9')
+					if (i == '9' && j == '8' && k == '9' && l == '9')
 						continue;
 					putchar(',');
 					putchar(' ');
+					}
 				}
 			}
 		}
