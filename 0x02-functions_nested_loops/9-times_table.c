d6c96408f8d5cc2fc901b117e79c42cfdacfb854
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-05 13:56:36 -0700

    Correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,14 +6,22 @@
  */
 void times_table(void)
 {
-	char i, j;
+	int i, j;
 
-	for (i = '0'; i <= '9'; ++i)
+	for (i = 0; i <= 9; ++i)
 	{
-		for (j = '0'; j <= '9'; ++j)
+		for (j = 0; j <= 9; ++j)
 		{
-			_putchar(i * j);
-			if (j == '9')
+			if (i * j <= 9)
+			{
+				_putchar('0' + (i * j));
+			}
+			else
+			{
+				_putchar('0' + ((i * j) / 10));
+				_putchar('0' + ((i * j) % 10));
+			}
+			if (j == 9)
 			{
 				_putchar('\n');
 				break;
