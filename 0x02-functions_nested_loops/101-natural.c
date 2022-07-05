9ebe6074c973656b0e38906b966f5c515aea5412
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-05 16:10:46 -0700

    Added the 101-natural.c source code

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 on Success
+ */
+int main(void)
+{
+	int i, sum;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 5 == 0 || i % 3 == 0)
+			sum += i;
+		printf("%d\n",sum);
+	}
+	return (0);
+}
