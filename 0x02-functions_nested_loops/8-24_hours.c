4ffe9e59ff6ea589f90aa60df67dc471657b96a2
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-05 13:09:21 -0700

    Added the 8-24_hours.c source code

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ */
+void jack_bauer(void)
+{
+	char i, j, k, l;
+
+	for (i = '0', i <= '2', ++i)
+	{
+		for (j = '0'; j <= '9'; ++j)
+		{
+			for (k = '0'; k <= '5'; ++k)
+			{
+				for (l = '0'; l <= '9'; ++l)
+				{
+					_putchar(i);
+					_putchar(j);
+					_putchar(':');
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+					if (i == '2' && j == '3' && k == '5' && l == '9')
+						break;
+				}
+			}
+		}
+	}
+}
