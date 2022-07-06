452856af1d18fb5e546d9d5549a92d900d8eb229
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-05 17:12:39 -0700

    Correction

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "main.h"
+#include <math.h>
 
 /**
  * main - Entry point
@@ -8,5 +8,21 @@
  */
 int main(void)
 {
+	int i;
+	long double sum = 0, num1 = 0, num2 = 1;
+
+	for (i = 1; i <= 50; i++)
+	{
+		sum = num1 + num2;
+		printf("%.0Lf", sum);
+		if (i == 50)
+		{
+			printf("\n");
+			break;
+		}
+		printf(", ");
+		num1 = num2;
+		num2 = sum;
+	}
 	return (0);
 }
