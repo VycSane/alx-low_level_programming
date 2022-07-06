953d7c049d084e6663e9a40e8e667f822e8a427b
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-06 05:07:38 -0700

    Correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,37 +3,32 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0.
- * @n: the argument n
  */
-void print_times_table(int n)
+void times_table(void)
 {
 	int i, j;
 
-	if (n < 0 || n > 15)
-		return;
-	for (i = 0; i <= n; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= n; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			if ((i * j) <= 9)
+			if ((i == 0 && j == 0) || (j == 0))
 			{
-				_putchar('0' + (i * j));
+				_putchar('0');
+			}
+			else if (i * j > n)
+			{
+				_putchar(' ');
+				_putchar('0' + (i * j) / 10);
+				_putchar('0' + (i * j) % 10);
 			}
 			else
 			{
-				_putchar('0' + ((i * j) / 10));
-				_putchar('0' + ((i * j) % 10));
-			}
-			if (j == n)
-			{
-				_putchar('\n');
-				continue;
-			}
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			if (i * j <= n)
 				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + i * j);
+			}
+			j == n ? _putchar('\n') : _putchar(',');
 		}
 	}
 }
