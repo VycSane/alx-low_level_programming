6075df9efad7e598cef13aef976402102eec2f87
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-05 18:17:31 -0700

    Correction

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,23 +8,18 @@
  */
 int main(void)
 {
-	int i;
-
-	long double sum = 0, num1 = 0, num2 = 1, total = 0;
+	unsigned long long int sum = 0, num1 = 0, num2 = 1, total = 0;
 
 	while (1)
 	{
 		sum = num1 + num2;
 		if (sum >= 4000000)
-		{
-			printf("\n");
 			break;
-		}
 		if (sum % 2 == 0)
 			total += sum;
 		num1 = num2;
 		num2 = sum;
 	}
-	printf("%.0Lf\n", total);
+	printf("%lld\n", total);
 	return (0);
 }
