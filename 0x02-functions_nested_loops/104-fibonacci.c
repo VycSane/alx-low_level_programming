2ce7cd76a4e472f779a820f6a0a4bc59451cafc5
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-05 17:23:18 -0700

    Added the 104-fibonacci.c source code

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	long double sum = 0, num1 = 0, num2 = 1;
+
+	for (i = 0; i < 98; i++)
+	{
+		sum = num1 + num2;
+		printf("%.0Lf", sum);
+		if (i == 97)
+		{
+			printf("\n");
+			break;
+		}
+		printf(", ");
+		num1 = num2;
+		num2 = sum;
+	}
+	return (0);
+}
