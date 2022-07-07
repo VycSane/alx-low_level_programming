8e6836b3ad0a62f942cbf628b9ae131aa0cf8d12
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-07 08:43:46 -0700

    Added the 100-prime_factor.c source code

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on Success
+ */
+int main(void)
+{
+	int i = 0;
+	unsigned pf = 2u;	
+	long long int n = 612852475143;
+
+	while (1)
+	{
+		if (n == 1)
+			break;
+		if (n == i)
+			i = 2;
+		if ((n % pf) == 0)
+			n /= pf;
+		else
+			pf++;
+	}
+	printf("%u\n", pf);
+	return (0);
+}
