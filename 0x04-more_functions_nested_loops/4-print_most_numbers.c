da7e8c97dbc84cb791999e280d6845c23dba08ab
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-07 07:06:14 -0700

    Correction

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line, excluding 2 and 4.
+ * print_most_numbers - prints the numbers, from 0 to 9, excluding 2 and 4.
  */
 void print_most_numbers(void)
 {
