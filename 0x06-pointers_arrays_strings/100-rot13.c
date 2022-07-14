961e2ce7225937e6c646fa9fe6e86c30b319ef5f
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-14 10:12:35 -0700

    Added the 100-rot13.c source code

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @str: input string
+ *
+ * Return: a pointer to string
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char tmp;
+
+	char key[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char value[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		tmp = str[i];
+		for (j = 0; j < strlen(key); j++)
+		{
+			if (tmp == key[j])
+				str[i] = value[j];
+		}
+	}
+	return (str);
+}
