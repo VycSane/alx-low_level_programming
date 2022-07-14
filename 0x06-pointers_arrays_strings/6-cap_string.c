4e2d1ca004fffc8248fd3e85fde74f378094b8b0
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-14 08:23:46 -0700

    Added the 6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: the input string
+ *
+ * Return: a pointer to the string.
+ */
+char *cap_string(char *str)
+{
+	int i;
+	char tmp;
+	char terminators[] = ",;.!?\"(){}\t\n ";
+	char next_chr;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		tmp = str[i];
+		next_chr = str[i + 1];
+		if (strchr(terminators, tmp) != NULL && next_chr != '\0')
+		{
+			if (next_chr >= 97 && next_chr <= 122)
+				str[i + 1] = next_chr - 32;
+		}
+	}
+	return (str);
+}
