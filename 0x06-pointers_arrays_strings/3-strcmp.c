898275b613759c8e0b16054db062411bd2853387
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-15 02:18:02 -0700

    Correction

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,9 +15,9 @@ int _strcmp(char *s1, char *s2)
 		if (s1[i] == '\0' && s2[i] == '\0')
 			return (0);
 		if (s1[i] > s2[i])
-			return (1);
+			return (13);
 		if (s2[i] > s1[i])
-			return (-1);
+			return (-13);
 		i++;
 	}
 }
