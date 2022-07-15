02b89431abdbf1a99d08ed956ad10f3050cf8b03
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-14 18:46:31 -0700

    Added the 3-strcmp.c source code

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+/**
+ * _strcmp - compares two strings.
+ * @s1: first string arg
+ * @s2: second string arg
+ *
+ * Return: 0 if strings are equal. 15 if s1 is greater.
+ * -15 if s2 is greater.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; ; i++)
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			return (0);
+		if (s1[i] > s2[i])
+			return (15);
+		if (s2[i] > s1[i])
+			return (-15);
+	}
+}
