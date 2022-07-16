afc6685fc6a3d278363da547652b5bc7c25c462b
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-15 19:17:25 -0700

    Correction

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,37 +11,21 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int d1, d2, sum, n, k;
+	int d1, d2, sum;
 
 	int i = (int) strlen(n1) - 1;
 	int j = (int) strlen(n2) - 1;
 	int carry = 0;
-
-	if (i >= j)
-		k = i + 1;
-	else
-		k = j + 1;
+	int k = i >= j ? (i + 1) : (j + 1);
 
 	if (size_r <= k + 1)
 		return (0);
-
-	n = k + 1;
-	while (n < size_r)
-	{
-		r[n] = '\0';
-		n++;
-	}
+	r[k + 1] = '\0';
 
 	while (i >= 0 || j >= 0)
 	{
-		if (i >= 0)
-			d1 = n1[i] - '0';
-		else
-			d1 = 0;
-		if (j >= 0)
-			d2 = n2[i] - '0';
-		else
-			d2 = 0;
+		d1 = i >= 0 ? n1[i] - '0' : 0;
+		d2 = j >= 0 ? n2[j] - '0' : 0;
 		sum = d1 + d2 + carry;
 		r[k] = (sum % 10) + 48;
 		carry = sum / 10;
