82ba804aaa60a3ce9286c524a7bfd4668b4f444b
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-19 16:52:34 -0700

    Added the 4-pow_recursion.c source code

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _pow_recursion - returns the value of x raised to the power of y.
+ * @x: the base
+ * @y: the power
+ *
+ * Return: an integer
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, (y - 1)));
+}
