50d53e7ef57f0c86695c49fb801ac4e6ff5035bf
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-19 14:47:38 -0700

    Correction

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,19 +7,26 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j;
-	unsigned int m = 0;
+	unsigned int counter = 0, i = 0, j = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	if (s[i] == '\0' && accept[j] == '\0')
+		return (0);
+
+	while (s[i] != '\0')
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		while (1)
 		{
 			if (s[i] == accept[j])
 			{
-				m++;
+				counter++;
 				break;
 			}
+			if (accept[j] == '\0')
+				return (counter);
+			j++;
 		}
+		j = 0;
+		i++;
 	}
-	return (m);
+	return (counter);
 }
