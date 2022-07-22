b11335a9c7a918cd6ea942094e017538dea9bc2b
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-22 06:02:14 -0700

    Added the 1-args.c source code

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: number of commandline args
+ * @argv: command line args
+ *
+ * Return: 0 on Success
+ */
+int main(int argc,__attribute__((unused)) char* argv[])
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}
