d83d33451ed51c0be39db528a05ddda1f343983e
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-26 05:40:07 -0700

    Added the 100-argstostr.c source code

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+/**
+ * argstostr - concatenates all the args to string.
+ * @ac: number of commandline args
+ * @av: array of commandline strings
+ *
+ * Return: a pointer to the new string or NULL.
+ */
+char *argstostr(int ac, char **av)
+{
+	char *cp;
+	size_t i = 1, j = 0, k = 0, len = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (i < ac)
+	{
+		len += (strlen(av[i]) + 1);
+		i++;
+	}
+	len++;
+	cp = (char *)malloc(len * sizeof(char));
+	if (cp == NULL)
+		return (NULL);
+	i = 1;
+	while (i < (size_t) ac)
+	{
+		j = 0;
+		while (j < strlen(av[i]))
+		{
+			cp[k] = av[i][j];
+			j++, k++;
+		}
+		cp[k] ='\n';
+		i++, k++;
+	}
+	cp[len - 1] = '\0';
+	return (cp);
+}
