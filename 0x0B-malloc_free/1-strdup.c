e664d1f4016f117d24078a2fbef3221804759a73
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-07-25 18:57:26 -0700

    Added the 1-strdup.c source code

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+/**
+ * _strdup - duplicates a string
+ * @str: string arg
+ *
+ * Return: a pointer to the string duplicate or NULL.
+ */
+char *_strdup(char *str)
+{
+	char *cp;
+	size_t i = 0, len = strlen(str) + 1;
+
+	if (str == NULL)
+		return (NULL);
+	cp = (char *)malloc(sizeof(char) * len);
+
+	if (cp == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		cp[i] = str[i];
+		i++;
+	}
+	return (cp);
+}
