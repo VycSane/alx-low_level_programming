fbce6751f401b097aa11d5c3d8fb2f75cfe6a052
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-08-02 00:44:12 -0700

    Correction

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,8 +10,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL || name == NULL || owner == NULL)
-		exit(1);
+	if (d == NULL)
+		exit(0);
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
