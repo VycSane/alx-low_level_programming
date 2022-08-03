72d36365f14aa0899d8e58990eed239759fa201d
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-08-03 07:31:13 -0700

    Correction

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,6 +13,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	if (size <= 0)
 		return (-1);
+	if (array == NULL || cmp == NULL)
+		exit(0);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
