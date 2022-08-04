1863beb1bbee77a137fc418b3d706810bd603101
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-08-03 17:22:24 -0700

    Correction

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -10,17 +10,20 @@
  */
 int main(int argc, char *argv[])
 {
-	int n = atoi(argv[1]);
+	int n;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
+	n = atoi(argv[1]);
 	if (n < 0)
 	{
 		printf("Error\n");
 		exit(2);
-	}
+	};
+	system("gcc 100-main_opcodes.c");
+	system("objdump -d a.out | awk -F\"\n\" -v RS=\"\n\n\" '$1 ~ /main/' | cut -f 2 |");
 	return (0);
 }
