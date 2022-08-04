d952fc23e7f99a6c004d6bfeb8b3c0810e1e1592
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-08-04 05:06:04 -0700

    Correction

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,7 +24,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			i++;
 			continue;
 		}
-		printf("%s", separator);
+		if (i < n - 1)
+			printf("%s", separator);
 		i++;
 	}
 	printf("\n");
