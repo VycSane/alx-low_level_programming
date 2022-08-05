760a5eb3b0f36890365759a6684b1bf531c0b9f3
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-08-04 17:38:32 -0700

    Correction

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,56 +1,41 @@
-#include <string.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <string.h>
 /**
- * print_all - prints anything
- * @format: specified formats
- * @...: list of variable args
+ * print_all - prints anything.
+ * @format: formats specified
+ * @...: variable args
  */
 void print_all(const char * const format, ...)
 {
-	int n = (int) strlen(format), i = 0, var_int, is_valid = 0;
-	char var_char, *var_str, f;
-	float var_float;
+	size_t i = 0, f_len = strlen(format);
+	char *var_str;
 	va_list args;
 
 	va_start(args, format);
-	while (i < n)
+	while (i < f_len)
 	{
-		f = format[i];
-		if (is_valid && (f == 'c' || f == 'f' || f == 's' || f == 'i'))
-			printf(", ");
-		switch (f)
+		switch (format[i])
 		{
 			case 'c':
-				var_char = (char) va_arg(args, int);
-				printf("%c", var_char);
-				is_valid = 1;
-				break;
-			case 'i':
-				var_int = va_arg(args, int);
-				printf("%d", var_int);
-				is_valid = 1;
+				printf("%c", va_arg(args, int));
 				break;
 			case 'f':
-				var_float = (float) va_arg(args, double);
-				printf("%f", var_float);
-				is_valid = 1;
+				printf("%f", va_arg(args, double));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
 				break;
 			case 's':
 				var_str = va_arg(args, char *);
 				if (var_str == NULL)
 				{
 					printf("(nil)");
-					is_valid = 1;
 					break;
 				}
 				printf("%s", var_str);
-				is_valid = 1;
-				break;
-			default:
-				is_valid = 0;
 		}
 		i++;
 	}
