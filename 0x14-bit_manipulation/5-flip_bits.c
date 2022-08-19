24732faed9d1de638de6378b982807690d18e7a4
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-08-18 19:35:38 -0700

    Added the 5-flip_bits.c file

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * flip_bits - counts number of bits to flip to get to another number
+ * @n: the number to be flipped
+ * @m: the number to get after flipping
+ *
+ * Return: the number of bits to be flipped
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int num = n ^ m, flip_count = 0;
+
+	while (num != 0)
+	{
+		num &= (num - 1);
+		flip_count++;
+	}
+	return (flip_count);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,4 +8,5 @@ int _putchar(char c);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 int get_endianness(void);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 #endif /* MAIN_H */
