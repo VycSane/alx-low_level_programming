eef41cf4066c68a386dd4ebe793812f6520f1fb3
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-08-22 21:57:27 -0700

    Added task 4 correction

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -15,10 +15,17 @@
  */
 int main(int argc, char *argv[])
 {
+	int fd, rs;
+	char buffer[1024];
+
 	if (argc != 2)
 	{
 		write(STDERR_FILENO, "Usage: elf_header elf_filename\n", 31);
 		exit(98);
 	}
+	fd = open(argv[1], O_RDONLY);
+	rs = read(fd, buffer, 64);
+	printf("%s\n", buffer);
+	close(fd);
 	return (0);
 }
