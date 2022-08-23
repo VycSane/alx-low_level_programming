548c77741eee2b8300da313a1394ba611ea2103b
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-08-22 21:41:48 -0700

    Added task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -15,15 +15,10 @@
  */
 int main(int argc, char *argv[])
 {
-	char command[1024];
-
 	if (argc != 2)
 	{
 		write(STDERR_FILENO, "Usage: elf_header elf_filename\n", 31);
 		exit(98);
 	}
-	strcpy(command, "readelf -h ");
-	strcat(command, argv[1]);
-	system(command);
 	return (0);
 }
