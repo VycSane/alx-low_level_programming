f652fae9b76bb3722c72e56fca769089aee54cfd
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-08-22 18:55:02 -0700

    Added task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+/**
+ * create_file - creates a text file
+ * @filename: the specified filename
+ * @text_content: the content that would be written to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, ws;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+		ws = write(fd, text_content, strlen(text_content));
+	if (ws == -1)
+		return (-1);
+	return (1);
+}
