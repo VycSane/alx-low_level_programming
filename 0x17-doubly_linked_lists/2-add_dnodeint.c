c02bbbe309a4729216287014a436b0bf739bb799
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-09-29 05:05:03 -0700

    Correction on task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,15 +17,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
-	if (*head == NULL)
-	{
-		new_node->next = NULL;
-	}
-	else
-	{
-		new_node->next = *head;
+	new_node->next = *head;
+	if (*head != NULL)
 		(*head)->prev = new_node;
-	}
 	*head = new_node;
 	return (new_node);
 }
