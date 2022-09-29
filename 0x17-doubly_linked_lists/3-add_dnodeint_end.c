6d97845b9651ebf08424db0d5c3249af44968b20
Ebede Ugochukwu Victor <ugoebede@gmail.com>
2022-09-29 05:38:01 -0700

    Added task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -21,7 +21,10 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->next = NULL;
 	tmp = *head;
 	if (tmp == NULL)
+	{
 		new_node->prev = NULL;
+		*head = new_node;
+	}
 	while (tmp != NULL)
 	{
 		if (tmp->next == NULL)
